3505703a30ae24a78650eaa3b0541a630c2fe81e
badblli <bsnradblli@gmail.com>
2022-04-01 14:13:21 +0300

    deploy

---
+++ components/icons/Behance.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const BehanceIcon = ({ className }) => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      className={className}
+    >
+<path d="M1670 2249 l0 -210 233 3 c227 3 234 4 282 29 68 34 98 87 98 174 0 48 -6 73 -24 105 -49 88 -137 110 -431 110 l-158 0 0 -211z"/>
+
+    </svg>
+  );
+};
+
+export default BehanceIcon;
